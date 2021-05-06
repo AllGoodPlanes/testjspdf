840eb04a1773ab1b2671a2e13ccd75a64a0609ae
Ric PE Long <richardlong@soriaconsulting.co.uk>
2021-05-06 11:24:21 +0100

    adj

--- main.go
+++ main.go
@@ -9,19 +9,17 @@ import (
     "log"
 )
 
-const STATIC_URL string = "/static/"
-const STATIC_ROOT string = "static/"
-
 func Home(w http.ResponseWriter, r *http.Request) {
-  lp := filepath.Join("templates","base.html")
-fp := filepath.Join("templates", "index.html")
-	t, err := template.ParseFiles(lp, fp)
+//  lp := filepath.Join("templates","base.html")
+//fp := filepath.Join("templates", "index.html")
+fp := filepath.Join("index.html")
+	t, err := template.ParseFiles(fp)
 	if err!=nil{
 		fmt.Println("it's here1")
 		log.Fatalln(err)
 	}
 
-err =  t.ExecuteTemplate(w, "base", "")
+err =  t.ExecuteTemplate(w, "index.html", "")
 if err != nil {
 		fmt.Println("it's here2")
 	log.Fatalln(err)
@@ -33,28 +31,9 @@ func main() {
 
 	fmt.Println("Listening...")
 	mux.HandleFunc("/", Home)
-    fs:= http.FileServer(http.Dir("static"))
-	mux.Handle("/static/",http.StripPrefix("/static/", fs))
-    dn:= http.FileServer(http.Dir("pdf.js"))
-	mux.Handle("/pdf.js/",http.StripPrefix("/pdf.js/", dn))
     http.ListenAndServe(GetPort(), mux)
-}
-
-func render(w http.ResponseWriter, tmpl string) {
-	tmpl_list := []string{"templates/base.html",
-		fmt.Sprintf("templates/%s.html", tmpl),
+    
 	}
-	t, err := template.ParseFiles(tmpl_list...)
-	if err != nil {
-		log.Print("template parsing error: ", err)
-	}
-	err= t.ExecuteTemplate(w, "base", "")
-	if err != nil {
-		log.Print("template executing error: ", err)
-	}
-
-}
-
 
 func GetPort() string {
 	port := os.Getenv("PORT")
