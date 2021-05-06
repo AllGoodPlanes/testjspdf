f28e2db9ad52859df46fa2aee745d44c6387fe48
Ric PE Long <richardlong@soriaconsulting.co.uk>
2021-05-06 15:02:57 +0100

    simple server to  test pdf.js

--- main.go
+++ main.go
@@ -1,48 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "path/filepath"
-    "html/template"
-    "os"
     "log"
+    "net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-//  lp := filepath.Join("templates","base.html")
-//fp := filepath.Join("templates", "index.html")
-fp := filepath.Join("index.html")
-lp := filepath.Join("web/viewer.html")
-	t, err := template.ParseFiles(fp,lp)
-	if err!=nil{
-		fmt.Println("it's here1")
-		log.Fatalln(err)
-	}
-
-err =  t.ExecuteTemplate(w, "index.html", "")
-if err != nil {
-		fmt.Println("it's here2")
-	log.Fatalln(err)
-}
-}
-
 func main() {
-   mux := http.NewServeMux()
 
-	fmt.Println("Listening...")
-	mux.HandleFunc("/", Home)
-    http.ListenAndServe(GetPort(), mux)
-    
-	}
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, r.URL.Path[1:])
+    })
 
-func GetPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	fmt.Println("Info: No port detected in the environment, defaulting to :" + port)
+    log.Fatal(http.ListenAndServe(":8081", nil))
 
-	return ":" + port
 }
+
 
